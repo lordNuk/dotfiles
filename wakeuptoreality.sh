18c4a052b4ca81cc919aa7a705f2a8fb87ce2e98
negimanish501 <negimanish501@gmail.com>
2023-11-23 18:06:13 +0530

    retest 2

--- wakeuptoreality.sh
+++ wakeuptoreality.sh
@@ -1,46 +1,50 @@
 #!/bin/bash
 
+echo "enter username: " username
+echo "enter password: " password
+
 echo "installing required packages..."
-pacman --noconfirm -S neovim tmux lolcat cowsay ttf-liberation-mono-nerd dunst feh xorg-xrandr network-manager-applet blueman
+echo $password | sudo -S pacman --noconfirm -S neovim tmux lolcat cowsay ttf-liberation-mono-nerd dunst feh xorg-xrandr network-manager-applet blueman
 
 echo "adding files: .bashrc, .profile, .dmenurc"
-rm /home/$USER/.bashrc /home/$USER/.profile /home/$USER/.fehbg /home/$USER/.dmenurc /home/$USER/.tmux.conf
-ln -s /home/$USER/dotfiles/.bashrc /home/$USER/
-ln -s /home/$USER/dotfiles/.profile /home/$USER/
-ln -s /home/$USER/dotfiles/.fehbg /home/$USER/
-ln -s /home/$USER/dotfiles/.dmenurc /home/$USER/
-ln -s /home/$USER/dotfiles/.tmux.conf /home/$USER/
+echo $password | sudo -S rm /home/$username/.bashrc /home/$username/.profile /home/$username/.fehbg /home/$username/.dmenurc /home/$username/.tmux.conf
+ln -s /home/$username/dotfiles/.bashrc /home/$username/
+ln -s /home/$username/dotfiles/.profile /home/$username/
+ln -s /home/$username/dotfiles/.fehbg /home/$username/
+ln -s /home/$username/dotfiles/.dmenurc /home/$username/
+ln -s /home/$username/dotfiles/.tmux.conf /home/$username/
 
 echo "checking for required directories..."
-if [ ! -d "/home/$USER/Pictures" ]; then
-    mkdir /home/$USER/Pictures
-    echo "created /home/$USER/Pictures directory"
+if [ ! -d "/home/$username/Pictures" ]; then
+    mkdir /home/$username/Pictures
+    echo "created /home/$username/Pictures directory"
 fi
-if [ -d "/home/$USER/Pictures/wallpapers" ]; then
-    rm -rf /home/$USER/Pictures/wallpapers
+if [ -d "/home/$username/Pictures/wallpapers" ]; then
+    rm -rf /home/$username/Pictures/wallpapers
 fi
-if [ ! -d "/home/$USER/.config" ]; then
-    mkdir /home/$USER/.config
-    echo "created /home/$USER/.config directory"
+if [ ! -d "/home/$username/.config" ]; then
+    mkdir /home/$username/.config
+    echo "created /home/$username/.config directory"
 fi
 
-rm -rf .config/nvim .config/i3 .config/i3status .config/dunst
+rm -rf /home/$username/.config/nvim /home/$username/.config/i3 /home/$username/.config/i3status /home/$username/.config/dunst
 
 echo "creating symlinks... "
-ln -s /home/$USER/dotfiles/wallpapers /home/$USER/Pictures/
-echo "- /home/$USER/dotfiles/wallpapers /home/$USER/Pictures/"
-ln -s /home/$USER/dotfiles/i3 /home/$USER/.config/
-echo "- /home/$USER/dotfiles/i3 /home/$USER/.config/"
-ln -s /home/$USER/dotfiles/i3status /home/$USER/.config/
-echo "- /home/$USER/dotfiles/i3status /home/$USER/.config/"
-ln -s /home/$USER/dotfiles/nvim /home/$USER/.config/
-echo "- /home/$USER/dotfiles/nvim /home/$USER/.config/"
-ln -s /home/$USER/dotfiles/dunst /home/$USER/.config/
-echo "- /home/$USER/dotfiles/dunst /home/$USER/.config/"
+ln -s /home/$username/dotfiles/wallpapers /home/$username/Pictures/
+echo "- /home/$username/dotfiles/wallpapers /home/$username/Pictures/"
+ln -s /home/$username/dotfiles/i3 /home/$username/.config/
+echo "- /home/$username/dotfiles/i3 /home/$username/.config/"
+ln -s /home/$username/dotfiles/i3status /home/$username/.config/
+echo "- /home/$username/dotfiles/i3status /home/$username/.config/"
+ln -s /home/$username/dotfiles/nvim /home/$username/.config/
+echo "- /home/$username/dotfiles/nvim /home/$username/.config/"
+ln -s /home/$username/dotfiles/dunst /home/$username/.config/
+echo "- /home/$username/dotfiles/dunst /home/$username/.config/"
 
-# echo "installing yay"
-# pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
-# cd ..
+echo "installing yay"
+cd /home/$username
+echo $password | sudo -S pacman -S --noconfirm --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+cd ..
 echo "installing required aur packages.. "
 yay -S --noconfirm mercury-browser
 
